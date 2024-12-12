ca3aed74d6dfbf74baea3b754b9025681f5b4b63
juan manuel <juanpersonalf@gmail.com>
2024-12-12 18:01:42 -0400

    actualizacion

--- src/js/detallesApis.js
+++ src/js/detallesApis.js
@@ -1,59 +1,47 @@
-const description = document.getElementById('description')
-const dataDescription = document.getElementById('dataDescription')
-function GetApis(){
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get("id");
+const description = document.getElementById("description");
+const dataDescription = document.getElementById("dataDescription");
+function GetApis() {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get("id");
 }
 
-async function datelli(){
-    const id = GetApis()
+async function datelli() {
+  const id = GetApis();
 
-   try
-   {
-    const response = await fetch("https://api.npoint.io/5aed6cb2e07430ef14a4/informacionTrading");
-      const elementos = await response.json();
-      const elemento = elementos.find((el) => el.id == id);
-      if(elemento){
-        const bodys = elemento.body.replace(/\n/g, "<br/>")
-        description.innerHTML = `<button id="stop" class="stops"><-</button><img src="${elemento.img}" class="look"/> <h1 class="title">${elemento.title}</h1> <div class="detailtitle">${bodys}</div>`
-        document.getElementById('stop').addEventListener('click',()=>{
-          window.location.href='../../index.html'
-        })
-      }
-
-   }catch{
-
-   }
-
-
+  try {
+    const response = await fetch(
+      "https://api.npoint.io/5aed6cb2e07430ef14a4/informacionTrading"
+    );
+    const elementos = await response.json();
+    const elemento = elementos.find((el) => el.id == id);
+    if (elemento) {
+      const bodys = elemento.body.replace(/\n/g, "<br/>");
+      description.innerHTML = `<button id="stop" class="stops"><-</button><img src="${elemento.img}" class="look"/> <h1 class="title">${elemento.title}</h1> <div class="detailtitle">${bodys}</div>`;
+      document.getElementById("stop").addEventListener("click", () => {
+        window.location.href = "../../index.html";
+      });
+    }
+  } catch {}
 }
-datelli()
+datelli();
 
+async function datellipersonal() {
+  const id = GetApis();
 
-
-
-
-async function datellipersonal(){
-    const id = GetApis()
-
-   try
-   {
-    const response = await fetch("https://api.npoint.io/5aed6cb2e07430ef14a4/datosTrading");
-      const elementos = await response.json();
-      const elemento = elementos.find((el) => el.id == id);
-      if(elemento){
-        const bodys = elemento.body.replace(/\n/g, "<br/>")
-        dataDescription.innerHTML = `<button id="stop" class="stops"><-</button><img src="${elemento.img}" class="look"/> <h1 class="title">${elemento.title}</h1> <div class="detailtitle">${bodys}</div> `
-        document.getElementById('stop').addEventListener('click',()=>{
-          window.location.href='../../index.html'
-        })
-      }
-
-   }catch{
-
-   }
-
-
+  try {
+    const response = await fetch(
+      "https://api.npoint.io/5aed6cb2e07430ef14a4/datosTrading"
+    );
+    const elementos = await response.json();
+    const elemento = elementos.find((el) => el.id == id);
+    if (elemento) {
+      const bodys = elemento.body.replace(/\n/g, "<br/>");
+      dataDescription.innerHTML = `<button id="stop" class="stops"><-</button><img src="${elemento.img}" class="look"/> <h1 class="title">${elemento.title}</h1> <div class="detailtitle">${bodys}</div> `;
+      document.getElementById("stop").addEventListener("click", () => {
+        window.location.href = "../../index.html";
+      });
+    }
+  } catch {}
 }
 
-datellipersonal()
+datellipersonal();
